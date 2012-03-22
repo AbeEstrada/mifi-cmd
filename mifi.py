c9b1e2b593a62b9b8f58f634fdffb2ad46c1936d
Abraham Estrada <abraham.estrada@gmail.com>
2012-03-22 15:31:19 -0600

    added more features

--- mifi.py
+++ mifi.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import urllib2
 
-class Mifi:
+class Mifi(object):
     status = False
 
     def __init__(self):
         url = 'http://192.168.1.1/getStatus.cgi?dataType=TEXT'
+
         try:
             response = urllib2.urlopen(url)
             data = response.read()
@@ -21,14 +22,32 @@ class Mifi:
                 self.network = f[1]
             elif f[0] == 'WwNetwkTech':
                 self.technology = f[1]
+            elif f[0] == 'WwRoaming':
+                if int(f[1]) == 0:
+                    self.roaming = 'Not Roaming'
+                elif int(f[1]) == 1:
+                    self.roaming = 'Roaming'
+                elif int(f[1]) == 2:
+                    self.roaming = 'Extended Network'
             elif f[0] == 'WwRssi':
-                self.signal = f[1]
+                self.signal = int(f[1])
+            elif f[0] == 'WwIpAddr':
+                self.ip = f[1]
+            elif f[0] == 'BaBattStat':
+                self.battery = int(f[1])
+            elif f[0] == 'BaBattChg':
+                if int(f[1]) == 0:
+                    self.charging = 'Not Charging'
+                elif int(f[1]):
+                    self.charging = 'Charging'
 
 def main():
     mifi = Mifi()
     if mifi.status:
-        print '%s %s' % (mifi.network, mifi.technology)
-        print 'Signal: %s' % (mifi.signal)
+        print '%s %s (%s)' % (mifi.network, mifi.technology, mifi.roaming)
+        print 'Signal: %i' % (mifi.signal)
+        print '%s' % (mifi.ip)
+        print 'Battery: %s (%s)' % (mifi.battery, mifi.charging)
 
 if __name__ == '__main__':
     main()
